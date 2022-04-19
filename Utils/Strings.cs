c551e66a18b29a12466f0ad97fe84d88d530c4ac
tlitookilakin <swyrlyy@gmail.com>
2022-04-19 15:12:17 -0400

    remove unnecessary conversion

--- Utils/Strings.cs
+++ Utils/Strings.cs
@@ -144,7 +144,7 @@ namespace AeroCore.Utils
                 yield break;
         }
         public static List<string> SafeSplitList(this ReadOnlySpan<char> s, char delim, bool RemoveEmpty = false) => 
-            (from item in s.SafeSplit(delim, RemoveEmpty) select item.ToString()).ToList();
+            s.SafeSplit(delim, RemoveEmpty).ToList();
         public static IEnumerable<string> SafeSplit(this string s, char delim, bool RemoveEmpty = false) => s.AsSpan().SafeSplit(delim, RemoveEmpty);
         public static List<string> SafeSplitList(this string s, char delim, bool RemoveEmpty = false) => s.AsSpan().SafeSplitList(delim, RemoveEmpty);
 

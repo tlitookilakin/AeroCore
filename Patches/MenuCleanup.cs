ee14ab4c68d876766532ac0973cfb36f1c97ff10
tlitookilakin <swyrlyy@gmail.com>
2022-05-31 14:12:35 -0400

    added disposal patch

---
+++ Patches/MenuCleanup.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using StardewValley;
+using System;
+
+namespace AeroCore.Patches
+{
+    [HarmonyPatch]
+    internal class MenuCleanup
+    {
+        [HarmonyPatch(typeof(Game1), nameof(Game1.exitActiveMenu))]
+        [HarmonyPrefix]
+        [HarmonyPriority(Priority.First)]
+        internal static void Prefix()
+        {
+            if (Game1.activeClickableMenu is IDisposable d)
+                d.Dispose();
+        }
+    }
+}
